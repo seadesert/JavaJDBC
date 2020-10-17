97697fdf5dfd62b53f5e34991cfb83d39920d618
ArashiDesu <47567561+ArunSriKrishna@users.noreply.github.com>
2020-10-17 14:12:44 +0530

    Update JDBC_Connection.java

--- src/JDBC_Classes/JDBC_Connection.java
+++ src/JDBC_Classes/JDBC_Connection.java
@@ -11,7 +11,7 @@ public class JDBC_Connection {
     public static void JDBC_getconnection() throws Exception 
     {
       String myDriver = "com.mysql.jdbc.Driver";
-      String myUrl = "jdbc:mysql://localhost:3306/mydb";
+      String myUrl = "jdbc:mysql://localhost:3306/emp";
       Class.forName(myDriver);
       Connection conn = DriverManager.getConnection(myUrl, "root", "");
     }
