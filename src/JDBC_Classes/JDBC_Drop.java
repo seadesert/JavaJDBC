e6880a3fcc1850c5a32fa267caca74654d4d92c7
ArashiDesu <47567561+ArunSriKrishna@users.noreply.github.com>
2020-10-17 13:26:20 +0530

    Added JDBC_Drop class

---
+++ src/JDBC_Classes/JDBC_Drop.java
@@ -0,0 +1,23 @@
+package JDBC_Classes;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Nagashree MK
+ */
+public class JDBC_Drop 
+{
+
+    Statement stmt; 
+   
+    //function to drop table
+    public static void Table_Delete() throws SQLException
+    {
+
+
+
+    }
+
+}
